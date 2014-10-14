ab4d186e91f1f015a8de26082fc30abff65475ae
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 20:33:53 -0300

    Efetua o primeiro teste, garantindo que uma categoria esteja realmente sendo salva no banco de dados

---
+++ WebAPI/SPAwesome.WebAPI/SPAwesome.Tests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPAwesome.Tests.DataBaseMocks;
+using SPAwesome.WebAPI.Controllers;
+using SPAwesome.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPAwesome.Tests.Controllers
+{
+    [TestClass]
+    public class CategoryControllerTests : AbstractInMemoryDataFixture
+    {
+        CategoryController categoryController = null;
+
+        [TestInitialize]
+        public void Setup()
+        {
+
+            categoryController = new CategoryController(base.Session);
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            Category category = new Category
+            {
+                Name = "teste",
+                Order = 1,
+                Slug = "teste"
+            };
+
+            var newcategory = categoryController.Post(category);
+
+            Assert.AreNotEqual(0, newcategory.Id);
+        }
+    }
+}
