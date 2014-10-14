ae3bb643e2b064a30281f9fe6bd73dcfcde286c6
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 20:32:54 -0300

    Classes base e abstratas para 'mockar' o comportamento do banco de dados

---
+++ WebAPI/SPAwesome.WebAPI/SPAwesome.Tests/DataBaseMocks/AbstractInMemoryDataFixture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPAwesome.Tests.DataBaseMocks
+{
+    /// <summary>
+    /// Classe abstrata que irá abstrair o processo de criar a ISession em todo novo teste
+    /// </summary>
+    public abstract class AbstractInMemoryDataFixture
+    {
+        private ISession session;
+
+        [TestInitialize]
+        public void BaseSetup()
+        {
+            session = InMemorySessionFactoryProvider.Instance.OpenSession();
+        }
+
+        [TestCleanup]
+        public void BaseTearDown()
+        {
+            if (session != null)
+                session.Dispose();
+        }
+
+        protected ISession Session
+        {
+            get { return session; }
+        }
+    }
+}
---
+++ WebAPI/SPAwesome.WebAPI/SPAwesome.Tests/DataBaseMocks/InMemorySessionFactoryProvider.cs
@@ -0,0 +1,69 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Bytecode;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Tool.hbm2ddl;
+using SPAwesome.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPAwesome.Tests.DataBaseMocks
+{
+    public class InMemorySessionFactoryProvider
+    {
+        private static InMemorySessionFactoryProvider instance;
+        public static InMemorySessionFactoryProvider Instance
+        {
+            get { return instance ?? (instance = new InMemorySessionFactoryProvider()); }
+        }
+
+        private ISessionFactory sessionFactory;
+        private Configuration configuration;
+
+        private InMemorySessionFactoryProvider()
+        {
+            this.Initialize();
+        }
+
+
+        public void Initialize()
+        {
+            sessionFactory = CreateSessionFactory();
+        }
+
+        private ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                    .Database(SQLiteConfiguration.Standard.InMemory().ShowSql())
+                    .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Category>())
+                    .ExposeConfiguration(cfg => configuration = cfg)
+                    .BuildSessionFactory();
+        }
+
+        public ISession OpenSession()
+        {
+            ISession session = sessionFactory.OpenSession();
+
+            var export = new SchemaExport(configuration);
+            export.Execute(true, true, false, session.Connection, null);
+
+            return session;
+        }
+
+        public void Dispose()
+        {
+            if (sessionFactory != null)
+                sessionFactory.Dispose();
+
+            sessionFactory = null;
+            configuration = null;
+        }
+    }
+}
