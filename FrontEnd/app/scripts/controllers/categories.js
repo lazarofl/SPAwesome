84f471234a509e0bcf5a9132f127dfd679d039e4
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 03:40:54 -0300

    Aplicada view para categorias com estrutura básica de formulário de inclusão e listagem de categorias

--- FrontEnd/app/scripts/controllers/categories.js
+++ FrontEnd/app/scripts/controllers/categories.js
@@ -7,7 +7,20 @@
  * # CategoriesCtrl
  * Controller of the spawesomeApp
  */
-angular.module('spawesomeApp')
-  .controller('CategoriesCtrl', function ($scope, Categories) {
-    
-  });
+ angular.module('spawesomeApp')
+ .controller('CategoriesCtrl', function ($scope, Categories) {
+ 	$scope.categories;
+ 	$scope.errors = [];
+
+ 	$scope.init = function(){
+ 		Categories
+ 		.getCategories()
+ 		.success(function(data){
+ 			$scope.categories = data;
+ 		}).
+ 		error(function(error){
+ 			$scope.errors.push(error);
+ 		});
+ 		
+ 	};
+ });
