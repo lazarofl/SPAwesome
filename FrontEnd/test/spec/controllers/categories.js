cbbd88c36a8933c428bdbb233bdeb2afedf31057
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 03:40:10 -0300

    injetei a dependência real na controller e apliquei um teste para verificar se caso ao requisitar com sucesso a lista de categorias a mesma é ou não adicionada no escopo categories'

--- FrontEnd/test/spec/controllers/categories.js
+++ FrontEnd/test/spec/controllers/categories.js
@@ -6,23 +6,53 @@ describe('Controller: CategoriesCtrl', function () {
   beforeEach(module('spawesomeApp'));
 
   var CategoriesCtrl,
-    httpBackend,
-    _Categories,
-    scope;
+  httpBackend,
+  Categories,
+  scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend, _Categories_) {
     scope = $rootScope.$new();
-    _Categories = {};
+    Categories = _Categories_;
     CategoriesCtrl = $controller('CategoriesCtrl', {
       $scope: scope,
-      Categories: _Categories
+      Categories: _Categories_
     });
     httpBackend = $httpBackend;
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(_Categories).not.toBe(null);
-    expect(_Categories).not.toBe(undefined);
+  it('Serviço Categories não pode ser nulo ou undefined', function () {
+    expect(Categories).not.toBe(null);
+    expect(Categories).not.toBe(undefined);
   });
+
+  it('Ao executar init() deve inserir as categorias no escopo "categories"', function () {
+    //setup
+    httpBackend.expectGET().respond(200,
+     [ 
+     {
+        Id: 1,
+        Name: 'Teste 1',
+        Order: 1,
+        Slug: 'teste-1'
+     },
+     {
+        Id: 2,
+        Name: 'Teste 2',
+        Order: 2,
+        Slug: 'teste-2'
+     }
+     ]
+   );
+
+    //act
+    scope.init();
+    httpBackend.flush();
+
+    //expected
+    expect(scope.categories.length).toBe(2);
+  });
+
+
+
 });
