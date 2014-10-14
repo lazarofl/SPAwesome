4b8165bce0541816e00579d8dc7215ddf6f4cbb4
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-13 22:23:27 -0300

    Executado 'yo angular SPAwesome' para criar a estrutura do projeto

---
+++ FrontEnd/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spawesomeApp
+ * @description
+ * # spawesomeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spawesomeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
