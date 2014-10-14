fb9907bf36d6f016881447f1647f5a882cc85e4b
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 08:40:19 -0300

    Padronizado o formato de alertas

--- FrontEnd/app/scripts/controllers/categories.js
+++ FrontEnd/app/scripts/controllers/categories.js
@@ -10,23 +10,32 @@
  angular.module('spawesomeApp')
  .controller('CategoriesCtrl', function ($scope, Categories) {
  	$scope.categories;
- 	$scope.errors = [];
+ 	$scope.alerts = [];
  	$scope.loading = false;
  	$scope.showadd = false;
  	$scope.name = '';
 
+
+ 	$scope.addAlert = function(message, type) {
+ 		$scope.alerts.push({type: type, msg: message});
+ 	};
+
+ 	$scope.closeAlert = function(index) {
+ 		$scope.alerts.splice(index, 1);
+ 	};
+
  	$scope.init = function(){
  		$scope.loading = true;
  		Categories
- 			.getCategories()
-			.success(function(data){
- 				$scope.categories = data;
- 				$scope.loading = false;
-			}).
- 			error(function(error){
- 				$scope.errors.push(error);
- 				$scope.loading = false;
- 			});
+ 		.getCategories()
+ 		.success(function(data){
+ 			$scope.categories = data;
+ 			$scope.loading = false;
+ 		}).
+ 		error(function(error){
+ 			$scope.addAlert('Não foi possível conectar na API, verifique a disponibilidade do serviço', 'danger');
+ 			$scope.loading = false;
+ 		});
  	};
 
  	$scope.addCategory = function(){
@@ -37,17 +46,18 @@
  		};
 
  		Categories
- 			.insertCategory(category)
-			.success(function(data){
- 				$scope.init();
+ 		.insertCategory(category)
+ 		.success(function(data){
+ 			$scope.init();
 
- 				$scope.name = '';
- 				$scope.showadd = false;
-			}).
- 			error(function(error){
- 				$scope.errors.push(error);
- 				$scope.loading = false;
- 			});
+ 			$scope.addAlert($scope.name + ' adicionado com sucesso','success');
+ 			$scope.name = '';
+ 			$scope.showadd = false;
+ 		}).
+ 		error(function(error){
+ 			$scope.addAlert(error.message, 'danger');
+ 			$scope.loading = false;
+ 		});
 
  	};
  });
