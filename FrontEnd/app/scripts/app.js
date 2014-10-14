21cff384178700bf6cd1d1b75bc11b05b1e65833
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 01:26:09 -0300

    Adicionada a estrutura da página de gestão de categorias, essa página será a página inicial da aplicação

--- FrontEnd/app/scripts/app.js
+++ FrontEnd/app/scripts/app.js
@@ -20,12 +20,8 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+        templateUrl: 'views/categories.html',
+        controller: 'CategoriesCtrl'
       })
       .otherwise({
         redirectTo: '/'
---
+++ FrontEnd/app/scripts/controllers/categories.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spawesomeApp.controller:CategoriesCtrl
+ * @description
+ * # CategoriesCtrl
+ * Controller of the spawesomeApp
+ */
+angular.module('spawesomeApp')
+  .controller('CategoriesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
