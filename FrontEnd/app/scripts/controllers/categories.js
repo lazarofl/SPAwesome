c0508d714cab5d2ec64d0a7d6ef1889a97677cec
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 08:18:48 -0300

    View de categorias recebeu um formulário e elementos de interface para carregamento e interação com usuário via formulário

--- FrontEnd/app/scripts/controllers/categories.js
+++ FrontEnd/app/scripts/controllers/categories.js
@@ -11,16 +11,43 @@
  .controller('CategoriesCtrl', function ($scope, Categories) {
  	$scope.categories;
  	$scope.errors = [];
+ 	$scope.loading = false;
+ 	$scope.showadd = false;
+ 	$scope.name = '';
 
  	$scope.init = function(){
+ 		$scope.loading = true;
  		Categories
- 		.getCategories()
- 		.success(function(data){
- 			$scope.categories = data;
- 		}).
- 		error(function(error){
- 			$scope.errors.push(error);
- 		});
- 		
+ 			.getCategories()
+			.success(function(data){
+ 				$scope.categories = data;
+ 				$scope.loading = false;
+			}).
+ 			error(function(error){
+ 				$scope.errors.push(error);
+ 				$scope.loading = false;
+ 			});
+ 	};
+
+ 	$scope.addCategory = function(){
+ 		$scope.loading = true;
+
+ 		var category = {
+ 			Name: $scope.name
+ 		};
+
+ 		Categories
+ 			.insertCategory(category)
+			.success(function(data){
+ 				$scope.init();
+
+ 				$scope.name = '';
+ 				$scope.showadd = false;
+			}).
+ 			error(function(error){
+ 				$scope.errors.push(error);
+ 				$scope.loading = false;
+ 			});
+
  	};
  });
