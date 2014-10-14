19a7421e83e23dce8abebd545c19338cf7927123
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-10-14 20:59:44 -0300

    Efetuado o Mock da ISession para que ela retorne obtejos simulados para o teste que tem o objetivo de garantir apenas que a session efetivamente execute a criteria

--- WebAPI/SPAwesome.WebAPI/SPAwesome.Tests/Controllers/CategoryControllerTests.cs
+++ WebAPI/SPAwesome.WebAPI/SPAwesome.Tests/Controllers/CategoryControllerTests.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NHibernate;
 using SPAwesome.Tests.DataBaseMocks;
 using SPAwesome.WebAPI.Controllers;
 using SPAwesome.WebAPI.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,12 +21,43 @@ namespace SPAwesome.Tests.Controllers
         [TestInitialize]
         public void Setup()
         {
-
             categoryController = new CategoryController(base.Session);
         }
 
+
         [TestMethod]
-        public void Add()
+        public void Get()
+        {
+            //ao inves de adicionar posts vou simular apenas o comportamento de GET para manter o SRP - Single Responsability Principle nesse exemplo
+            // utilizando o Moq para alterar o comportamento da ISession
+
+            var fakeCategories = new List<Category>();
+            for (int i = 0; i < 10; i++)
+            {
+                fakeCategories.Add(new Category
+                {
+                    Name = string.Format("Teste {0}", i + 1),
+                    Order = i + 1
+                });
+            }
+
+            //setup
+            Mock<ISession> sessionMock = new Mock<ISession>();
+            sessionMock.Setup(x => x.CreateCriteria<Category>().List<Category>())
+                .Returns(fakeCategories);
+
+            //altera a session
+            categoryController = new CategoryController(sessionMock.Object);
+
+
+            //act
+            var categories = categoryController.Get().ToList();
+
+            Assert.AreEqual(fakeCategories.Count, categories.Count);
+        }
+
+        [TestMethod]
+        public void Post()
         {
             Category category = new Category
             {
